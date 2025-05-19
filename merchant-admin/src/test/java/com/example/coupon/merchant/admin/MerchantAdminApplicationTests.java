50197f090ed5429176ba8748152d75e74fb7733c
zicca <ziq@zjut.edu.cn>
2025-05-19 11:00:47 +0800

    新建merchant-admin模块

---
+++ merchant-admin/src/test/java/com/example/coupon/merchant/admin/MerchantAdminApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.coupon.merchant.admin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MerchantAdminApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
